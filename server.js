8db3a38025798deb7bac04fdc41e9b452a336c99
soniya2002 <96329774+soniya2002@users.noreply.github.com>
2024-01-18 23:16:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from "url";
 
 //configure env
 dotenv.config();
+
+//es module
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //databse config
 connectDB();
